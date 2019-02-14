dced859837a3b638617bb67759b2405e9dddc2eb
frann-y <fgranger00.com>
2019-02-14 19:46:55 +0000

    Displaying Incoming Messages, made a unigue id for messages, dsplayed them to all clients. broad cast

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
-
+const uuidv4 = require('uuid/v4');
 // Set the port to 3001
 const PORT = 3001;
 
@@ -19,17 +19,71 @@ const wss = new SocketServer({ server });
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 //setting up server to send and recieve text data
-wss.on('connection', (ws) => {
-  console.log('Client connected');
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    // if (client.readyState === SocketServer.OPEN) {
+      client.send(data);
+    // }
+  });
+};
 
-  ws.on('message', data => {
-    const message = JSON.parse(data);
-    console.log(`User ${message.username} said ${message.content}`);
+
+  wss.on('connection', (ws) => {
+    console.log('Client connected');
+
+    ws.on('message', data => {
+    const clientMessage = JSON.parse(data);
+    //console.log(`User ${clientMessage.username} said ${clientMessage.content}`);
+
+    switch (clientMessage.type) {
+      case 'postMessage':
+    //   //copying current object and giving i and send back
+        const outgoingMessage = {
+          ...clientMessage,
+          id: uuidv4(), 
+          type: 'incomingMessage',
+        };
+        wss.broadcast(JSON.stringify(outgoingMessage));
+        break;
+      default:
+        console.log('Unknown Type of Message!');
+    }
   });
 
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
-  
-});
+    // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+    ws.on('close', () => console.log('Client disconnected'));
+    
+  });
 
+// //original code
+// wss.on('connection', (ws) => {
+//   console.log('Client connected');
+
+//   ws.on('message', data => {
+//     const message = JSON.parse(data);
+//     console.log(`User ${message.username} said ${message.content}`);
+//   });
+
+//   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+//   ws.on('close', () => console.log('Client disconnected'));
+  
+
+//   //written code??
+// wsClient.on('message', data => {
+//   const message = JSON.parse(data);
+//   onsole.log(`User ${message.username} said ${message.content}`);
+
+//   switch (clientMessage.type) {
+//     case 'postNotification':
+//       const outgoingMessage = {
+//         ...clientMessage,
+//         id: uuidv4(),
+//         type: 'incomingNotification',
+//       };
+//       wss.broadcast(JSON.stringify(outgoingMessage));
+//       break;
+//     default:
+//       console.log('Unknow Type of Message!');
+//   }
+// });
 
--- src/App.jsx
+++ src/App.jsx
@@ -20,17 +20,19 @@ class App extends Component {
           username: 'Janet',
         },
 
-        messages: [{ 
-          id: uuid.v1(), 
+        messages: [
+        //old messages hardcoded
+        { id: uuid.v1(), 
           username: "Bob", 
-          content: "Pickles"
-        }],
+          content: "Pickles"}],
       };
   }
 
     //update the msg list
     //info gets passed up from chatbar
     //call render and new state is passed to children   
+
+
     updateUsername = username => {
       // this.sendUpdateUsername(this.state.currentUser, username); //finish
       this.setState({
@@ -47,19 +49,32 @@ class App extends Component {
      let newMsg = {
        id: uuid(), 
        username: this.state.currentUser.username, 
-       content: message
+       content: message,
+       type: 'postMessage',
       };
 
-      this.setState({
-        messages: [ 
-          ...this.state.messages, 
-          newMsg ],
-      });
+      // this.setState({
+      //   messages: [ 
+      //     ...this.state.messages, 
+      //     newMsg ],
+      // });
 
       this.myWebSocket.send(JSON.stringify(newMsg)); 
 
     };
 
+    //Adding Notification for ws
+    addNewMessage = msg => {
+      const message = {
+        id: msg.id,
+        username: msg.username,
+        content: msg.content,     
+      };
+      let newMessages = this.state.messages.concat(message);
+
+      this.setState({messages: newMessages});
+      console.log(`adding ${JSON.stringify(message)}`);
+    };
 
   componentDidMount() {
 
@@ -76,18 +91,25 @@ class App extends Component {
       this.myWebSocket.onmessage = message => {
         const serverMessage = JSON.parse(message.data);
         console.log("Connected to server")
-    }
+        switch (serverMessage.type) {
+          case 'incomingMessage':
+            this.addNewMessage(serverMessage);
+            break;
+          default:
+            console.log('Unknown message from server');
+        }
+      };
 
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    // console.log("componentDidMount <App />");
+    // setTimeout(() => {
+    //   console.log("Simulating incoming message");
+    //   // Add a new message to the list of messages in the data store
+    //   const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    //   const messages = this.state.messages.concat(newMessage)
+    //   // Update the state of the app component.
+    //   // Calling setState will trigger a call to render() in App and all child components.
+    //   this.setState({messages: messages})
+    // }, 3000);
   
 }
 
